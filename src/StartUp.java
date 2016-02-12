d22f94a4815a6bedadde603f54d300fc6a3d8923
Ruben Vermeulen <ruben.vermeulen@hotail.com>
2016-02-12 11:47:54 +0100

    Persistence unit toegevoegd

--- src/StartUp.java
+++ src/StartUp.java
@@ -1,9 +1,13 @@
 import domein.DomeinController;
+import domein.User;
 import gui.LoginFrameController;
 import gui.MainMenuFrameController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import util.JPAUtil;
 
 public class StartUp extends Application {
 
@@ -18,6 +22,26 @@ public class StartUp extends Application {
     }
 
     public static void main(String... args) {
-        Application.launch(StartUp.class, args);
+//        Application.launch(StartUp.class, args);
+
+        // Database test
+        EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+        
+        User user = new User();
+        
+        user.setFirstname("Ruben");
+        user.setLastname("Vermeulen");
+        user.setEmail("ruben.vermeulen.v4419@student.hogent.be");
+        
+        em.persist(user);
+        
+        em.getTransaction().commit();
+        
+        em.close();
+        emf.close();
+        System.out.println("Closed");
     }
 }
---
+++ src/util/JPAUtil.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author ruben
+ */
+public class JPAUtil {
+    private final static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
+    
+    private JPAUtil() {
+    }
+    
+    public static JPAUtil getInstance() {
+        return JPAUtilHolder.INSTANCE;
+    }
+    
+    private static class JPAUtilHolder
+    {
+
+        private static final JPAUtil INSTANCE = new JPAUtil();
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+}
