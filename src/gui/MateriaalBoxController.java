aa2762a5a15313f1b75ded7f5beab6ea5b48c3d6
Sven <Sven@192.168.0.234>
2016-02-15 20:54:39 +0100

    extra details kunnen zien van materialen

--- src/gui/MateriaalBoxController.java
+++ src/gui/MateriaalBoxController.java
@@ -3,19 +3,23 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-public class MateriaalBoxController extends HBox {
+public class MateriaalBoxController extends VBox {
 
     @FXML
     private ImageView imgvFoto;
@@ -37,6 +41,10 @@ public class MateriaalBoxController extends HBox {
     private Button btnBewerk;
     @FXML
     private Button btnVerwijder;
+    @FXML
+    private Button btnDetail;
+    @FXML
+    private GridPane materiaalDetailsBox;
 
     public MateriaalBoxController(BorderPane parent, String naam, String beschrijving){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MateriaalBox.fxml"));
@@ -50,6 +58,17 @@ public class MateriaalBoxController extends HBox {
         
         this.lblNaam.setText(naam);
         this.txtaBeschrijving.setText(beschrijving);
+        getChildren().remove(this.materiaalDetailsBox);
+    }
+    
+    @FXML
+    private void onDetailsAction(){
+        ObservableList<Node> children = getChildren();
+        if (children.contains(materiaalDetailsBox)){
+            children.remove(materiaalDetailsBox);
+        }else{
+            children.add(materiaalDetailsBox);
+        }
     }
     
 }
