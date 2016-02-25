df069c0b8c3f1ced81046f5599d102483ef6c33f
Xander Berkein <xander.berkein.v7629@student.hogent.be>
2016-02-25 13:47:11 +0100

    VoegMateriaalToe testen + bugfix converteerMateriaalViewNaarMateriaal

--- src/domein/MateriaalCatalogus.java
+++ src/domein/MateriaalCatalogus.java
@@ -5,6 +5,7 @@
  */
 package domein;
 
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import java.util.ArrayList;
 import java.util.List;
 import shared.MateriaalView;
@@ -20,6 +21,7 @@ public class MateriaalCatalogus {
 
     public MateriaalCatalogus() {
         firmaRepo = new FirmaRepository();
+        materialen = new ArrayList<>();
     }
 
     public void loadMaterialen(List<Materiaal> materialen) {
@@ -27,7 +29,7 @@ public class MateriaalCatalogus {
     }
 
     public Materiaal voegMateriaalToe(MateriaalView mv) {
-        
+
         String naam = mv.getNaam();
         int aantal = mv.getAantal();
         String firmaNaam = mv.getFirma();
@@ -41,27 +43,30 @@ public class MateriaalCatalogus {
         String plaats = mv.getPlaats();
         String doelgroepen = mv.getDoelgroepen();
         String leergebieden = mv.getLeergebieden();
-       
+
+        System.out.println(prijs);
         
         //Exceptions werpen
-        if(naam == null || naam.isEmpty())
+        if (naam == null || naam.isEmpty()) {
             throw new IllegalArgumentException("naam");
-        
-        if(aantal < 0){
+        }
+
+        if (aantal < 0) {
             throw new IllegalArgumentException("aantal");
         }
-        
-        if(firmaEmail != null && !firmaEmail.isEmpty() && !firmaEmail.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")){
+
+        if (firmaEmail != null && !firmaEmail.isEmpty() && !firmaEmail.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")) {
             throw new IllegalArgumentException("emailfirma");
         }
-        
-        if(prijs < 0)
+
+        if (prijs < 0) {
             throw new IllegalArgumentException("prijs");
-        
-        if(aantalOnbeschikbaar < 0)
+        }
+
+        if (aantalOnbeschikbaar < 0) {
             throw new IllegalArgumentException("onbeschikbaar");
-        
-        
+        }
+
         Materiaal materiaal = new Materiaal(naam, aantal);
 
         //check of opgegeven firma al bestaat in db
@@ -79,10 +84,10 @@ public class MateriaalCatalogus {
 
         //voeg materiaal toe aan repo
         materialen.add(materiaal);
-        
-        return(materiaal);
+
+        return (materiaal);
     }
-    
+
     public List<MateriaalView> geefAlleMaterialen() {
         List<MateriaalView> materiaalViews = new ArrayList();
 
@@ -92,13 +97,13 @@ public class MateriaalCatalogus {
 
         return materiaalViews;
     }
-    
+
     public void verwijderMateriaal(Materiaal materiaal) {
-        
+
         materialen.remove(materiaal);
-        
+
     }
-    
+
     public Materiaal geefMateriaal(String materiaalNaam) {
         Materiaal materiaal = null;
 
@@ -111,7 +116,7 @@ public class MateriaalCatalogus {
 
         return materiaal;
     }
-    
+
     public List<MateriaalView> geefMaterialenMetFilter(String filter) {
         if (filter == null || filter.isEmpty()) {
             return geefAlleMaterialen();
@@ -124,15 +129,14 @@ public class MateriaalCatalogus {
         }
         return matViews;
     }
-    
-    
+
     private MateriaalView convertMateriaalToMateriaalView(Materiaal m) {
         MateriaalView mv = new MateriaalView(m.getNaam(), m.getAantal());
         mv.setFotoUrl(m.getFoto()).setOmschrijving(m.getBeschrijving()).setArtikelNummer(m.getArtikelnummer())
                 .setAantalOnbeschikbaar(m.getAantalOnbeschikbaar()).setUitleenbaarheid(m.isUitleenbaarheid())
                 .setPlaats(m.getPlaats()).setFirma(m.getFirma().getNaam()).setEmailFirma(m.getFirma().getEmail())
-                .setDoelgroepen(m.getDoelgroepen()).setLeergebieden(m.getLeergebieden());
+                .setDoelgroepen(m.getDoelgroepen()).setLeergebieden(m.getLeergebieden()).setPrijs(m.getPrijs());
         return mv;
     }
-    
+
 }
---
+++ test/domein/MateriaalCatalogusTest.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domein;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import shared.MateriaalView;
+
+/**
+ *
+ * @author Xander
+ */
+public class MateriaalCatalogusTest {
+
+    private MateriaalCatalogus materiaalCatalogus;
+
+    private final String CORRECT_NAAM = "NaamTest";
+    private final int CORRECT_AANTAL = 15;
+
+    @Before
+    public void before() {
+        //materiaalcatalogus is leeg, zal altijd slechts 1 materiaal bevatten als men 1 materiaal toevoegd
+        materiaalCatalogus = new MateriaalCatalogus();
+
+    }
+
+    @Test
+    public void voegNieuwMateriaalToeEnkelNaamEnAantal() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, CORRECT_AANTAL);
+        materiaalCatalogus.voegMateriaalToe(mv);
+        assertTrue(compareMateriaalViews(mv, materiaalCatalogus.geefAlleMaterialen().get(0)));
+    }
+
+    @Test
+    public void voegNieuwMateriaalToeVolledigCorrect() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, CORRECT_AANTAL).setAantalOnbeschikbaar(CORRECT_AANTAL)
+                .setArtikelNummer("56465465sqf").setDoelgroepen("Doelgroep1, doelgroep2")
+                .setFotoUrl("C://pad/naar/url").setFirma("Firma").setEmailFirma("email@firma.be")
+                .setLeergebieden("Leergebied1,Leergebied2").setOmschrijving("Omschrijving")
+                .setPlaats("Plaats").setPrijs(2.22).setUitleenbaarheid(true);
+        materiaalCatalogus.voegMateriaalToe(mv);
+        assertTrue(compareMateriaalViews(mv, materiaalCatalogus.geefAlleMaterialen().get(0)));
+        assertEquals(mv.getPrijs(), materiaalCatalogus.geefAlleMaterialen().get(0).getPrijs(),0.001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void voegNieuwMateriaalToeNaamIsEmpty() {
+        MateriaalView mv = new MateriaalView("", CORRECT_AANTAL);
+        materiaalCatalogus.voegMateriaalToe(mv);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void voegNieuwMateriaalToeAantalIsKleinerDanNul() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, -1);
+        materiaalCatalogus.voegMateriaalToe(mv);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void voegNieuwMateriaalToeAantalOnbeschikbaarIsKleinerDanNul() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, CORRECT_AANTAL).setAantalOnbeschikbaar(-1);
+        materiaalCatalogus.voegMateriaalToe(mv);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void voegNieuwMateriaalToeEmailGeenAtEnPunt() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, CORRECT_AANTAL).setEmailFirma("blablabla");
+        materiaalCatalogus.voegMateriaalToe(mv);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void voegNieuwMateriaalToeEmailVreemdeKarakters() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, CORRECT_AANTAL).setEmailFirma("xander.berkein@gmiail=.com");
+        materiaalCatalogus.voegMateriaalToe(mv);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void voegNieuwMateriaalToePrijsKleinerDanNul() {
+        MateriaalView mv = new MateriaalView(CORRECT_NAAM, CORRECT_AANTAL).setPrijs(-0.25);
+        materiaalCatalogus.voegMateriaalToe(mv);
+    }
+
+    public boolean compareMateriaalViews(MateriaalView mv1, MateriaalView mv2) {
+        return (mv1.getNaam().equals(mv2.getNaam()))
+                && (mv1.getAantal() == mv2.getAantal())
+                && (mv1.getFirma() == null ? mv2.getFirma() == null : mv1.getFirma().equals(mv2.getFirma()))
+                && (mv1.getEmailFirma() == null ? mv2.getEmailFirma() == null : mv1.getEmailFirma().equals(mv2.getEmailFirma()))
+                && (mv1.getFotoUrl() == null ? mv2.getFotoUrl() == null : mv1.getFotoUrl().equals(mv2.getFotoUrl()))
+                && (mv1.getOmschrijving() == null ? mv2.getOmschrijving() == null : mv1.getOmschrijving().equals(mv2.getOmschrijving()))
+                && (mv1.getArtikelNummer() == null ? mv2.getArtikelNummer() == null : mv1.getArtikelNummer().equals(mv2.getArtikelNummer()))
+                && (mv1.getAantalOnbeschikbaar() == mv2.getAantalOnbeschikbaar())
+                && (mv1.isUitleenbaarheid() == mv2.isUitleenbaarheid())
+                && (mv1.getPlaats() == null ? mv2.getPlaats() == null : mv1.getPlaats().equals(mv2.getPlaats()))
+                && (mv1.getDoelgroepen() == null ? mv2.getDoelgroepen() == null : mv1.getDoelgroepen().equals(mv2.getDoelgroepen()))
+                && (mv1.getLeergebieden() == null ? mv2.getLeergebieden() == null : mv1.getLeergebieden().equals(mv2.getLeergebieden()));
+    }
+
+
+}
