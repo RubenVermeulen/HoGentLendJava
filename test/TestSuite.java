513b71d1248f2b920de3eb2b6a354fa7fd2bf2aa
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-03-09 23:11:44 +0100

    Test suite aangemaakt.

---
+++ test/TestSuite.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author ruben
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    domein.DomeinControllerTest.class,
+    domein.firma.FirmaTest.class,
+    domein.firma.FirmaCatalogusTest.class,
+    domein.gebruiker.GebruikerTest.class,
+    domein.gebruiker.BeheerderCatalogusTest.class,
+    domein.groep.GroepTest.class,
+    domein.groep.GroepCatalogusTest.class,
+    domein.materiaal.MateriaalTest.class,
+    domein.materiaal.MateriaalCatalogusTest.class,    
+})
+public class TestSuite {
+    
+}
