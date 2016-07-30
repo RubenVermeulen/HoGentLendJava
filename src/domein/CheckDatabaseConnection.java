1590d8e891dc8060b85cbbc06fbbe0b54013c5b1
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-07-30 21:25:29 +0200

    Voegt melding toe als de verbinding met de database wegvalt

--- src/domein/CheckDatabaseConnection.java
+++ src/domein/CheckDatabaseConnection.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 
 public class CheckDatabaseConnection implements Runnable {
 
@@ -13,27 +14,37 @@ public class CheckDatabaseConnection implements Runnable {
     private final String userName = "remote";
     private final String password = "remote";
     private final String connectionString = "jdbc:sqlserver://192.168.56.101\\SQLEXPRESS:1433;databaseName=HoGentLend";
+    
     private Connection conn;
     private Statement stmt;
     private DomeinController dc;
+    
+    private boolean isBooting = true;
 
     public CheckDatabaseConnection(DomeinController dc) {
         this.dc = dc;
     }
-    
+
     @Override
     public void run() {
         createConnection();
-        
-        while (true) {              
-            try {               
+
+        while (true) {
+
+            System.out.println(dc.isConnectionAlive());
+
+            try {
                 stmt.executeQuery("SELECT 1");
                 dc.setConnectionAlive(true);
             } catch (SQLException ex) {
+                if (dc.isConnectionAlive()) {
+                    promptLostConnection();
+                }
+
                 dc.setConnectionAlive(false);
                 createConnection();
             }
-            
+
             try {
                 Thread.sleep(interval);
             } catch (InterruptedException ex) {
@@ -41,13 +52,27 @@ public class CheckDatabaseConnection implements Runnable {
             }
         }
     }
-    
+
     private void createConnection() {
         try {
+            if (isBooting) {
+                promptLostConnection();
+                isBooting = false;
+            }
+
             conn = DriverManager.getConnection(connectionString, userName, password);
             stmt = conn.createStatement();
         } catch (SQLException ex) {
             createConnection();
         }
+    }
+
+    private void promptLostConnection() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                dc.promptLostConnection();
+            }
+        });
     }
 }
